8022625f32d21e169e3dd36162154e1ffb054df3
Alexi Badillo <AlexiBadilloOficial@gmail.com>
2023-07-03 14:45:44 -0600

    Primer proyecto terminado

--- Color aleatorio/app.js
+++ Color aleatorio/app.js
@@ -1,1 +1,19 @@
-alert('Hoola wapo, komostas'):
+// Seleccionar elementos del DOM
+const boton = document.querySelector('button');
+const color = document.getElementById('color');
+function generarColorHexAleatorio() {
+  let digitos = '0123456789ABCDEF';
+  let colorHex = '#';
+  for (let i = 0; i < 6; i++) {
+    let indiceAleatorio = Math.floor(Math.random() * 16);
+    colorHex += digitos[indiceAleatorio];
+  }
+  return colorHex;
+}
+boton.addEventListener('click', function() {
+  let colorAleatorio = generarColorHexAleatorio();
+  // Actualizar el texto
+  color.textContent = colorAleatorio;
+  // Cambiar el fondo
+  document.body.style.backgroundColor = colorAleatorio;
+});
